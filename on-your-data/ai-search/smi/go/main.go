84486623188fd2b92bd7bea3f2b6278394010866
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2024-07-09 21:30:29 +0000

    Adding sample for System Assigned Managed Identity.

---
+++ on-your-data/ai-search/smi/go/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/ai/azopenai"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	azureOpenAIEndpoint := os.Getenv("AZURE_OPENAI_ENDPOINT")
+	azureOpenAIDeployment := os.Getenv("AZURE_OPENAI_DEPLOYMENT")
+	azureSearchEndpoint := os.Getenv("AZURE_SEARCH_ENDPOINT")
+	azureSearchIndex := os.Getenv("AZURE_SEARCH_INDEX")
+	if azureOpenAIEndpoint == "" || azureOpenAIDeployment == "" {
+		fmt.Println("Please set AZURE_OPENAI_ENDPOINT and AZURE_OPENAI_KEY environment variables")
+		return
+	}
+
+	credential, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		fmt.Println("Failed to create a credential")
+		return
+	}
+
+	azureOpenAIClient, err := azopenai.NewClient(azureOpenAIEndpoint, credential, nil)
+	if err != nil {
+		fmt.Println("Failed to create Azure OpenAI client")
+		return
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What do you need?")
+
+	inputText, _ := reader.ReadString('\n')
+
+	options := azopenai.ChatCompletionsOptions{
+		Messages: []azopenai.ChatRequestMessageClassification{
+			&azopenai.ChatRequestUserMessage{
+				Content: azopenai.NewChatRequestUserMessageContent(inputText),
+			},
+		},
+		DeploymentName: &azureOpenAIDeployment,
+		AzureExtensionsOptions: []azopenai.AzureChatExtensionConfigurationClassification{
+			&azopenai.AzureSearchChatExtensionConfiguration{
+				Parameters: &azopenai.AzureSearchChatExtensionParameters{
+					Endpoint:       &azureSearchEndpoint,
+					IndexName:      &azureSearchIndex,
+					Authentication: &azopenai.OnYourDataSystemAssignedManagedIdentityAuthenticationOptions{},
+				},
+			},
+		},
+	}
+
+	response, err := azureOpenAIClient.GetChatCompletionsStream(context.Background(), options, nil)
+
+	if err != nil {
+		fmt.Println("Failed to get chat completions")
+		return
+	}
+
+	read_stream_response(response)
+
+}
+
+func read_stream_response(response azopenai.GetChatCompletionsStreamResponse) {
+	reader := response.ChatCompletionsStream
+	defer reader.Close()
+
+	for {
+		completion, err := reader.Read()
+		if err != nil {
+			break
+		}
+
+		if len(completion.Choices) > 0 && completion.Choices[0].Delta.Content != nil {
+
+			fmt.Print(*completion.Choices[0].Delta.Content)
+		}
+	}
+}
